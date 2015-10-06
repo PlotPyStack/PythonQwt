e1d77cb7eeead2dd45290604abed226fea2f9fec
Pierre Raybaut <pierre.raybaut@gmail.com>
2015-10-06 23:25:31 +0200

    Replaced remaining 'python-qwt' occurences

--- build_chm.bat
+++ build_chm.bat
@@ -1,13 +1,13 @@
 sphinx-build -b htmlhelp doc doctmp
-"C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\python-qwt.hhp
-"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\python-qwt.hhp
-copy doctmp\python-qwt.chm .
-7z a python-qwt.chm.zip python-qwt.chm
-del doctmp\python-qwt.chm
+"C:\Program Files\HTML Help Workshop\hhc.exe" doctmp\PythonQwt.hhp
+"C:\Program Files (x86)\HTML Help Workshop\hhc.exe" doctmp\PythonQwt.hhp
+copy doctmp\PythonQwt.chm .
+7z a PythonQwt.chm.zip PythonQwt.chm
+del doctmp\PythonQwt.chm
 del doc.zip
 sphinx-build -b html doc doctmp
 cd doctmp
 7z a -r ..\doc.zip *.*
 cd ..
 rmdir /S /Q doctmp
-del python-qwt.chm.zip
+del PythonQwt.chm.zip
--- doc/index.rst
+++ doc/index.rst
@@ -1,6 +1,17 @@
 .. automodule:: qwt
 
-Download :download:`CHM Documentation (unzip and open) <../PythonQwt.chm.zip>`.
+.. note::
+
+    Download :download:`CHM Manual <../PythonQwt.chm.zip>`.
+
+    After downloading this file, you may see blank pages in the documentation. 
+    That's because Windows is blocking CHM files for security reasons. 
+    Fixing this problem is easy:
+
+        * Right-click the CHM file, select properties, then click “Unblock”.
+        * Or compress the CHM file into a zip archive and decompress it in 
+          another directory.
+        * Do not open the CHM file on a network drive.
 
 Contents:
 
