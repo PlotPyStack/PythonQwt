e0efb4c58acab5b18715aee69bcc49f82fd46bd5
Pierre Raybaut <pierre.raybaut@gmail.com>
2015-10-08 23:14:25 +0200

    Updated CHANGELOG.md (0.3.0) and setup.py (long description)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # History of changes
 
+## Version 0.3.0
+
+Renamed the project (python-qwt --> PythonQwt), for various reasons.
+
 ## Version 0.2.1
 
 Fixed Issue #23: "argument numPoints is not implemented" error was showing up 
--- setup.py
+++ setup.py
@@ -33,24 +33,10 @@ no longer maintained. The idea was to translate the original Qwt C++ code to
 Python and then to optimize some parts of the code by writing new modules 
 based on NumPy and other libraries.
 
-
 The ``PythonQwt`` package consists of a single Python package named `qwt` 
-which is a pure Python implementation of Qwt C++ library with the following 
-limitations.
-
-The following `Qwt` classes won't be reimplemented in `qwt` because more
-powerful features already exist in `guiqwt`: `QwtPlotZoomer`, `QwtCounter`, 
-`QwtEventPattern`, `QwtPicker`, `QwtPlotPicker`.
-
-Only the following plot items are currently implemented in `qwt` (the only 
-plot items needed by `guiqwt`): `QwtPlotItem` (base class), `QwtPlotItem`, 
-`QwtPlotMarker`, `QwtPlotSeriesItem`, `QwtPlotHistogram`, `QwtPlotCurve`.
-
-The `QwtClipper` class is not implemented yet (and it will probably be 
-very difficult or even impossible to implement it in pure Python without 
-performance issues). As a consequence, when zooming in a plot curve, the 
-entire curve is still painted (in other words, when working with large 
-amount of data, there is no performance gain when zooming in)."""
+which is a pure Python implementation of Qwt C++ library with some 
+limitations: efforts were concentrated on basic plotting features, leaving 
+higher level features to the `guiqwt` library."""
 KEYWORDS = ''
 CLASSIFIERS = []
 if 'beta' in version or 'b' in version:
