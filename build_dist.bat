fb2ba30885324e024a9893d2bc6422c6648cb174
Pierre Raybaut <pierre.raybaut@gmail.com>
2015-11-14 18:14:14 +0100

    Updated build scripts

--- build_dist.bat
+++ build_dist.bat
@@ -5,4 +5,3 @@ python setup.py build bdist_wininst --plat-name=win32
 python setup.py build bdist_wininst --plat-name=win-amd64
 python setup.py sdist bdist_wheel --universal
 python setup.py build sdist
-pause
