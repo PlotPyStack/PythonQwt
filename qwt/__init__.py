70a66e23b37b4948b36d2686f367897ae6f09330
Pierre Raybaut <pierre.raybaut@gmail.com>
2024-06-19 18:26:50 +0200

    Bump qwt version to 0.12.4

--- qwt/__init__.py
+++ qwt/__init__.py
@@ -55,7 +55,7 @@ from qwt.symbol import QwtSymbol as QSbl  # see deprecated section
 from qwt.text import QwtText  # noqa: F401
 from qwt.toqimage import array_to_qimage as toQImage  # noqa: F401
 
-__version__ = "0.12.3"
+__version__ = "0.12.4"
 QWT_VERSION_STR = "6.1.5"
 
 
