44d90052a94612482ebc5dd723b12cf4187477d5
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2025-07-12 01:39:35 +0200

    [FIX] wrong handling of border.rectList with PySide6 backend
    This appears to fix the problems described in issue #102 .
    The changes can be made more pythonic, this patch literally came out
    of my debugging session - feedback regarding cleaning it up and
    making it ready for merging would be highly appreciated

--- qwt/plot_canvas.py
+++ qwt/plot_canvas.py
@@ -75,9 +75,16 @@ class QwtStyleSheetRecorder(QwtNullPaintDevice):
             # Pyside
             if isinstance(rects, (QRect, QRectF)):
                 self.border.list = [rects]
+            elif all(isinstance(rects, (QRect, QRectF)):
+                if isinstance(rects, list):
+                    self.border.rectList += rects
+                else:
+                    # That may not even be necessary
+                    self.border.rectList += [r for r in rects]
             else:
+                # Should we get here ?
                 for i in range(count):
-                    self.border.rectList += [rects.getRect().index(i)]
+                    self.border.rectList += [rects[i]]
         else:
             # PyQt
             for i in range(count):
